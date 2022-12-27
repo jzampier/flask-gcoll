6fad5ca73a4e7a13f8ac2e8461840743839bf28f
Julio Zampier <juliozampier@gmail.com>
2022-12-26 21:01:04 -0300

    Add templates and new_game.html page

--- game_coll.py
+++ game_coll.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+"""Flask Game Site"""
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -12,19 +13,51 @@ class Game:
         self.console = console
 
 
-@app.route('/begining')
-def hello():
+game1 = Game('World of Warcraft', 'MMORPG', 'PC')
+game2 = Game('League of Legends', 'MOBA', 'PC')
+game3 = Game('Unreal Tournament', 'FPS', 'PC')
+game4 = Game('Street Fighter II', 'Fight', 'Arcade')
+game_list = [
+    game1,
+    game2,
+    game3,
+    game4,
+]
+
+
+@app.route('/')
+def index():
     """_summary_
 
     Returns:
         _description_
     """
-    game1 = Game('World of Warcraft', 'MMORPG', 'PC')
-    game2 = Game('League of Legends', 'MOBA', 'PC')
-    game3 = Game('Unreal Tournament', 'FPS', 'PC')
-    game4 = Game('Street Fighter II', 'Fight', 'Arcade')
-    game_list = [game1, game2, game3, game4]
     return render_template('index.html', ttl='my game collection', games=game_list)
 
 
-app.run()
+@app.route('/new_game')
+def new():
+    """_summary_"""
+    return render_template('new_game.html', ttl='New Game')
+
+
+@app.route(
+    '/create',
+    methods=[
+        'POST',
+    ],
+)
+def create():
+    """_summary_"""
+    name = request.form.get('name')
+    category = request.form.get('category')
+    console = request.form.get('console')
+    game = Game(name, category, console)
+    game_list.append(game)
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#!4.6
